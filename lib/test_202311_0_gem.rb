ae0fb3d379f21b913e6e2fbc5f73a72a71298a5b
Yuya.Nishida <yuya@j96.org>
2023-11-29 10:33:58 +0900

    run: git cococo --init=test_202311_0_gem bundle gem test_202311_0_gem --no-mit

---
+++ lib/test_202311_0_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "test_202311_0_gem/version"
+
+module Test2023110Gem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/test_202311_0_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Test2023110Gem
+  VERSION = "0.1.0"
+end
